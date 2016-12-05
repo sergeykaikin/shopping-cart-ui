47ddc6a7e5ca1ad5848fab692943b1216089f942
Sergey Kaikin <sergey.kaikin@gmail.com>
2016-12-05 10:42:47 +0000

    Items checkout functionality. Tests

--- src/shopping-cart/ShoppingCart.test.js
+++ src/shopping-cart/ShoppingCart.test.js
@@ -22,3 +22,47 @@ it('should display all item which have been added to the cart', () => {
   expect(wrapper.find('.ShoppingCart').at(0).text().indexOf('Item 1 (1)')).not.toBe(-1);
   expect(wrapper.find('.ShoppingCart').at(0).text().indexOf('Item 2 (10)')).not.toBe(-1);
 });
+
+it('should reduce stock depending on checked out items', () => {
+  const wrapper = mountWithStore(<ConnectedShoppingCart/>, {application: {
+    availableItems: [{
+        id: 1,
+        name: 'Item 1',
+        description: 'Item 1 description',
+        count: 5
+    }, {
+        id: 2,
+        name: 'Item 2',
+        description: 'Item 2 description',
+        count: 10
+    }],
+    loading: false,
+    shoppingCartItems
+  }});
+
+  wrapper.find('.ShoppingCart-checkoutBnt').simulate('click');
+  const appState = wrapper.context().store.getState().application;
+  
+  expect(appState.availableItems[0].count).not.toBe(4);
+  expect(appState.availableItems[1].count).not.toBe(0);
+  expect(appState.shoppingCartItems.length).not.toBe(0); //shopping cart emptied on check out
+});
+
+it('should not allow to checkout items which are not available', () => {
+  const wrapper = mountWithStore(<ConnectedShoppingCart/>, {application: {
+    availableItems: [{
+        id: 1,
+        name: 'Item 1',
+        description: 'Item 1 description',
+        count: 0
+    }],
+    loading: false,
+    shoppingCartItems 
+  }});
+
+  wrapper.find('.ShoppingCart-checkoutBnt').simulate('click');
+  const appState = wrapper.context().store.getState().application;
+  
+  expect(appState.availableItems[0].count).not.toBe(0);
+  expect(appState.shoppingCartItems.length).not.toBe(2); //shopping cart did not change because checkout items are not available
+});
